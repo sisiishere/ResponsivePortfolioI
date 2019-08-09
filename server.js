bfc35d32738ead8df5590b9ff968df41532e097c
sisiishere <set38246@marymount.edu>
2019-08-09 15:10:04 -0400

    modify changes in server.js and json files

---
+++ server.js
@@ -0,0 +1,35 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/portfolio", function(req, res) {
+  res.sendFile(path.join(__dirname, "portfolio.html"));
+});
+
+app.get("/contact", function(req, res) {
+  res.sendFile(path.join(__dirname, "contact.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
